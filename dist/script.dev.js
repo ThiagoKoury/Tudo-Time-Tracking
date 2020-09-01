e4d2cdb98134bb49ee65631fd601d0b3c815e84c
Lucas Koury <5368350-lucas.jumpi@users.noreply.gitlab.com>
2020-09-01 12:23:07 -0300

    escrevi e saí correndo, pau no cu de quem ta lando

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,138 +1,100 @@
 "use strict";
 
-var lista = document.getElementsByTagName("li");
-var i;
-var hh = 0;
-var mm = 0;
-var ss = 0;
-var cron;
+/*jshint esversion: 6 */
+// Init task list
+var tasks = []; // Methods
 
-function timer() {
-  ss++;
+function secondsToTimeString(timeInSeconds) {
+  var hours = Math.floor(timeInSeconds / 3600);
+  var minutes = Math.floor(timeInSeconds / 60) % 60;
+  var seconds = Math.floor(timeInSeconds - hours * 3600 + minutes * 60) % 60;
+  var h = hours.toString().padStart(2, '0');
+  var i = minutes.toString().padStart(2, '0');
+  var s = seconds.toString().padStart(2, '0');
+  console.log(timeInSeconds);
+  return "".concat(h, ":").concat(i, ":").concat(s);
+}
 
-  if (ss == 59) {
-    ss = 0;
-    mm++;
+function renderTask(newTask) {
+  var baseTask = document.getElementById('base-task'); // Clone basetask element
 
-    if (mm == 59) {
-      mm = 0;
-      hh++;
-    }
+  var newTaskElement = baseTask.cloneNode(true); // Remove base task id
+
+  newTaskElement.id = "task-".concat(tasks.length); // Add Texts and time, ...
+
+  newTaskElement.setAttribute('data-id-tarefa', newTask.id);
+  document.getElementById('list-task').appendChild(newTaskElement);
+  document.querySelector("#".concat(newTaskElement.id, " .name")).innerText = newTask.name;
+  setInterval(function () {
+    return startTimeTrack(newTask.time);
+  }, 1000);
+
+  function startTimeTrack(second) {
+    document.querySelector("#".concat(newTaskElement.id, " .time")).innerText = secondsToTimeString(second);
+    second++;
   }
 
-  var formato = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
-  document.getElementsById("tempo").innerText = formato;
-  return formato;
+  addRemoveTaskListener(newTaskElement.id);
 }
 
-for (i = 0; i < lista.length; i++) {
-  var span = document.createElement("span");
-  var txt = document.createTextNode("\xD7");
-  span.className = "excluir";
-  span.appendChild(txt);
-  lista[i].appendChild(span);
-}
+function addTask(newTask) {
+  tasks.push(newTask);
+  renderTask(newTask);
+  console.log(tasks);
+} // Listeners
 
-var excluir = document.getElementsByClassName("excluir");
-var i;
 
-for (i = 0; i < excluir.length; i++) {
-  excluir[i].onclick = function () {
-    var div = this.parentElement;
-    div.style.display = "none";
-  };
-}
+function handleAddNewTaskButtonClick() {
+  var newTaskName = document.getElementById('new-task-name').value.trim();
 
-function reinicia() {
-  clearInterval(cron);
-  hh = 0;
-  mm = 0;
-  ss = 0;
-  document.getElementById('counter').innerText = '00:00:00';
-}
-
-function newElement() {
-  var li = document.createElement("li");
-  li.className = "elemento";
-  var varlorInput = document.getElementById("input").value;
-  var t = document.createTextNode(varlorInput);
-  li.appendChild(t);
-
-  if (varlorInput === '') {
-    alert("Digite um nome para a tarefa");
-  } else {
-    document.getElementById("tarefas").appendChild(li);
+  if (newTaskName === '') {
+    return alert('Invalid name');
   }
 
-  document.getElementById("input").value = "";
-  var tempo = document.createElement("SPAN");
-  var txtTempo = document.createTextNode("00:00:00");
-  tempo.id = "tempo";
-  tempo.appendChild(txtTempo);
-  li.appendChild(tempo);
-  var spanContinua = document.createElement("SPAN");
-  var txtContinua = document.createTextNode(">");
-  spanContinua.className = "continua";
-  spanContinua.appendChild(txtContinua);
-  li.appendChild(spanContinua);
-
-  spanContinua.onclick = function () {
-    this.style.display = "none";
-    this.nextSibling.style.display = "inline";
+  var newTask = {
+    id: Date.now(),
+    name: newTaskName,
+    time: 0,
+    paused: true,
+    completed: false
   };
-
-  var spanPausa = document.createElement("SPAN");
-  var txtPausa = document.createTextNode("||");
-  spanPausa.className = "pausa";
-  spanContinua.style.display = "none";
-  spanPausa.appendChild(txtPausa);
-  li.appendChild(spanPausa);
-
-  spanPausa.onclick = function () {
-    clearInterval(cron);
-    this.style.display = "none";
-    this.previousSibling.style.display = "inline";
-  };
-
-  var spanFim = document.createElement("SPAN");
-  var txtFim = document.createTextNode("v");
-  spanFim.className = "fim";
-  spanFim.appendChild(txtFim);
-  li.appendChild(spanFim);
-
-  spanFim.onclick = function () {
-    clearInterval(cron);
-    this.parentElement.style.backgroundColor = "lightgreen";
-    this.style.display = "none";
-    this.previousSibling.style.display = "none";
-    this.nextSibling.style.display = "inline";
-  };
-
-  var spanRe = document.createElement("SPAN");
-  var txtRe = document.createTextNode("<");
-  spanRe.className = "re";
-  spanRe.style.display = "none";
-  spanRe.appendChild(txtRe);
-  li.appendChild(spanRe);
-
-  spanRe.onclick = function () {
-    clearInterval(cron);
-    hh = 0;
-    mm = 0;
-    ss = 0;
-    setInterval(timer, 1000);
-  };
-
-  var spanExclui = document.createElement("SPAN");
-  var txtExclui = document.createTextNode("\xD7");
-  spanExclui.className = "excluir";
-  spanExclui.appendChild(txtExclui);
-  li.appendChild(spanExclui);
-
-  for (i = 0; i < excluir.length; i++) {
-    excluir[i].onclick = function () {
-      var div = this.parentElement;
-      div.style.display = "none";
-    };
-  }
+  addTask(newTask);
 }
+
+function findMatchParent(matchClassName, element) {
+  var currentElement = element;
+
+  while (currentElement.classList.has(matchClassName)) {
+    currentElement = currentElement.parent;
+  }
+
+  return currentElement;
+}
+
+function removeTask(taskId, taskElement) {
+  var taskIndex = tasks.findIndex(function (task) {
+    return task.id === parseInt(taskId, 10);
+  });
+
+  if (taskIndex === -1) {
+    return alert('Task not found');
+  }
+
+  tasks.splice(taskIndex, 1);
+  taskElement.remove();
+  console.log(tasks);
+}
+
+function addRemoveTaskListener(taskElementId) {
+  var taskElement = document.getElementById(taskElementId);
+  var taskId = taskElement.getAttribute('data-id-tarefa');
+  taskElement.addEventListener('click', function () {
+    return removeTask(taskId, taskElement);
+  });
+}
+
+window.onload = function () {
+  console.log('[TIME TRACKING] Start');
+  var addTaskButton = document.getElementById('add-task');
+  addTaskButton.addEventListener('click', handleAddNewTaskButtonClick);
+};
--- script.js
+++ script.js
@@ -1,154 +1,96 @@
-var lista = document.getElementsByTagName("li");
-var i;
-var hh = 0;
-var mm = 0;
-var ss = 0;
-var cron;
+/*jshint esversion: 6 */
 
-function timer() {
-  ss++;
+// Init task list
+const tasks = [];
 
-  if (ss == 59) { 
-      ss = 0; 
-      mm++; 
+// Methods
 
-      if (mm == 59) { 
-          mm = 0;
-          hh++;
-      }
-  }
-
-  
-  var formato = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
-  
-  
-  document.getElementsById("tempo").innerText = formato;
-
- 
-  return formato;
-}
-
-
-for (i = 0; i < lista.length; i++) {
-  var span = document.createElement("span");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "excluir";
-  span.appendChild(txt);
-  lista[i].appendChild(span);
-}
-
-var excluir = document.getElementsByClassName("excluir");
-var i;
-for (i = 0; i < excluir.length; i++) {
-  excluir[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  };
-}
-
-
-
-
-
-function reinicia() {
-  clearInterval(cron);
-  hh = 0;
-  mm = 0;
-  ss = 0;
-
-  document.getElementById('counter').innerText = '00:00:00';
-}
-
-function newElement() {
-  var li = document.createElement("li");
-  li.className = "elemento";
-  var varlorInput = document.getElementById("input").value;
-  var t = document.createTextNode(varlorInput);
-  li.appendChild(t);
-  if (varlorInput === '') {
-    alert("Digite um nome para a tarefa");
-  } else {
-    document.getElementById("tarefas").appendChild(li);
-  }
-  document.getElementById("input").value = "";
-
-
-
-  var tempo = document.createElement("SPAN");
-  var txtTempo = document.createTextNode("00:00:00");
-  tempo.id = "tempo";
-  tempo.appendChild(txtTempo);
-  li.appendChild(tempo);
-  
-  var spanContinua = document.createElement("SPAN");
-  var txtContinua = document.createTextNode(">");
-  spanContinua.className = "continua";
-  spanContinua.appendChild(txtContinua);
-  li.appendChild(spanContinua);
-  
-  spanContinua.onclick = function(){
-    this.style.display = "none";
-    this.nextSibling.style.display = "inline";
-  };
-
-
-  var spanPausa = document.createElement("SPAN");
-  var txtPausa = document.createTextNode("||");
-  spanPausa.className = "pausa";
-  spanContinua.style.display = "none";
-  spanPausa.appendChild(txtPausa);
-  li.appendChild(spanPausa);
-
-  spanPausa.onclick = function(){
-    clearInterval(cron);
-    this.style.display = "none";
-    this.previousSibling.style.display = "inline";
-  };
-
-
-
-  var spanFim = document.createElement("SPAN");
-  var txtFim = document.createTextNode("v");
-  spanFim.className = "fim";
-  spanFim.appendChild(txtFim);
-  li.appendChild(spanFim);
-  
-  spanFim.onclick = function(){
-    clearInterval(cron);
-    this.parentElement.style.backgroundColor = "lightgreen";
-    this.style.display = "none";
-    this.previousSibling.style.display = "none";
-    this.nextSibling.style.display = "inline";
-  };
-
-
-  var spanRe = document.createElement("SPAN");
-  var txtRe = document.createTextNode("<");
-  spanRe.className = "re";
-  spanRe.style.display = "none";
-  spanRe.appendChild(txtRe);
-  li.appendChild(spanRe);
-
-  spanRe.onclick = function(){
-    clearInterval(cron);
-    hh = 0;
-    mm = 0;
-    ss = 0;
-    setInterval(timer, 1000);
+function secondsToTimeString(timeInSeconds) {
     
-  };
-
-
-  var spanExclui = document.createElement("SPAN");
-  var txtExclui = document.createTextNode("\u00D7");
-  spanExclui.className = "excluir";
-  spanExclui.appendChild(txtExclui);
-  li.appendChild(spanExclui);
-
-  for (i = 0; i < excluir.length; i++) {
-    excluir[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    };
-  }
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor(timeInSeconds / 60) % 60;
+    const seconds = Math.floor(timeInSeconds - hours * 3600 + minutes * 60) % 60;
+    const h = hours.toString().padStart(2, '0');
+    const i = minutes.toString().padStart(2, '0');
+    const s = seconds.toString().padStart(2, '0');
+    console.log(timeInSeconds);
+    return `${h}:${i}:${s}`;
 }
+
+
+
+function renderTask(newTask) {
+    const baseTask = document.getElementById('base-task');
+    // Clone basetask element
+    const newTaskElement = baseTask.cloneNode(true);
+    // Remove base task id
+    newTaskElement.id = `task-${tasks.length}`;
+    // Add Texts and time, ...
+    newTaskElement.setAttribute('data-id-tarefa', newTask.id);
+    document.getElementById('list-task').appendChild(newTaskElement);
+    document.querySelector(`#${newTaskElement.id} .name`).innerText = newTask.name;
+    setInterval(() => startTimeTrack(newTask.time), 1000);
+    
+    function startTimeTrack(second){
+        document.querySelector(`#${newTaskElement.id} .time`).innerText = secondsToTimeString(second);
+        second++;
+    }
+
+    addRemoveTaskListener(newTaskElement.id);
+}
+
+function addTask(newTask) {
+    tasks.push(newTask);
+    renderTask(newTask);
+    console.log(tasks);
+}
+
+// Listeners
+
+function handleAddNewTaskButtonClick() {
+    const newTaskName = document.getElementById('new-task-name').value.trim();
+    if (newTaskName === '') {
+        return alert('Invalid name');
+    }
+    const newTask = {
+        id: Date.now(),
+        name: newTaskName,
+        time: 0,
+        paused: true,
+        completed: false
+    };
+    addTask(newTask);
+}
+
+function findMatchParent(matchClassName, element) {
+    let currentElement = element;
+    while (currentElement.classList.has(matchClassName)) {
+        currentElement = currentElement.parent;
+    }
+    return currentElement;
+}
+
+function removeTask(taskId, taskElement) {
+    const taskIndex = tasks.findIndex(function(task) {
+        return task.id === parseInt(taskId, 10);
+    });
+    if (taskIndex === -1) {
+        return alert('Task not found');
+    }
+    tasks.splice(taskIndex, 1);
+    taskElement.remove();
+    console.log(tasks);
+}
+
+function addRemoveTaskListener(taskElementId) {
+    const taskElement = document.getElementById(taskElementId);
+    const taskId = taskElement.getAttribute('data-id-tarefa');
+    taskElement.addEventListener('click', () => removeTask(taskId, taskElement));
+}
+
+window.onload = () => {
+    console.log('[TIME TRACKING] Start');
+
+    const addTaskButton = document.getElementById('add-task');
+    addTaskButton.addEventListener('click', handleAddNewTaskButtonClick);
+};
